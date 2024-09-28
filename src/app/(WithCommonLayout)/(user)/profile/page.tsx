71b6628bb7fe40a1503e09d6bf6469c47fc09b1a
Erazul Islam <erazul89@gmail.com>
2024-09-28 22:29:16 +0600

    create the authentication part

--- src/app/(WithCommonLayout)/(user)/profile/page.tsx
+++ src/app/(WithCommonLayout)/(user)/profile/page.tsx
@@ -1,11 +1,24 @@
+/* eslint-disable import/order */
+/* eslint-disable prettier/prettier */
+
+"use client"
+
+import { useUser } from '@/src/context/user.provider'
 import React from 'react';
 
-const page = () => {
+const UserProfile = () => {
+
+
+    const {user} = useUser()
+
     return (
         <div>
-            profile
+            <p>{user?.name}</p>
+            <p>{user?.email}</p>
+            <p>{user?.mobileNumber}</p>
+            <img className=' h-16 rounded-md' src={user?.profilePhoto} alt="" />
         </div>
     );
 };
 
-export default page;
+export default UserProfile;
