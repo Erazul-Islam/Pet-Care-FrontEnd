1fc20a77f7fda3f64a4f5186a540f6bbd3fc3b9b
Erazul Islam <erazul89@gmail.com>
2024-10-07 00:48:37 +0600

    added

--- src/components/posts/get.post.tsx
+++ src/components/posts/get.post.tsx
@@ -10,7 +10,7 @@ import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 
 import Payment from './payment';
-import { motion, useReducedMotion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useGetPost } from '@/src/hooks/get.post.hook';
 import { useAddComment, useDeleteComment, useEditComment } from '@/src/hooks/comment.hook';
 import { useUser } from '@/src/context/user.provider';
@@ -21,14 +21,10 @@ import PetMarkDownEditor from '@/src/app/(WithCommonLayout)/(user)/profile/_comp
 import { useGetUser } from '@/src/hooks/auth.hook';
 
 
-
-
-
 const GetPost = () => {
 
     const { data: posts, isSuccess, refetch, isFetching } = useGetPost();
-    const {data} = useGetUser()
-    console.log(data)
+    const { data: userData } = useGetUser()
 
     const { user } = useUser()
     const stripePromise = loadStripe('pk_test_51OEWQiI8i8m69lNjPL8a3QNQtS31dfaIR6lr00gHoVxSTvtZpjdNVv186ZG7pYGfTwqchyWoClqvbBLGmdzA4Oxr00lZCJmnc7')
@@ -90,8 +86,6 @@ const GetPost = () => {
         }
     };
 
-    console.log(user)
-
     const handleDeleteComment = async (postId: string, commentId: string) => {
         try {
             await deleteComment.mutateAsync({ postId, commentId });
@@ -143,7 +137,11 @@ const GetPost = () => {
             toast.error("You cannot unfollow yourself.");
             return;
         }
-        unfollow(targetUserId)
+        unfollow(targetUserId,{
+            onSuccess : () => {
+                window.location.reload()
+            }
+        })
     };
 
     const handleToggle = async () => {
@@ -179,98 +177,110 @@ const GetPost = () => {
                 <motion.div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
                     {
                         isSuccess &&
-                        sortedPosts?.map((post) => (
-                            <div key={post._id}>
-                                <div
-                                    className=" shadow-md mt-20 border border-purple-200 rounded-lg p-4 max-w-screen-md"
-                                >
-                                    <div className="flex justify-between items-center mb-4">
-                                        <div className="flex items-center space-x-3">
-                                            <img
-                                                alt="User avatar"
-                                                className="w-10 h-10 rounded-full"
-                                                src={post.userProfilePhoto}
-                                            />
-                                            <div>
-                                                <h4 className="">{post.userName}</h4>
-                                                <p className="text-xs">{new Date(post.createdAt).toLocaleString()}</p>
-                                                <p className="text-xs">total upvotes {post?.totalUpvotes}</p>
-                                                <p className="text-xs">total downvote {post?.totalDownvotes}</p>
-                                            </div>
-                                            <div>
-                                                {post?.isPremium === "YES" ? <img alt="" className='w-12 h-12 ' src="https://i.ibb.co.com/jM3xrDW/premium-quality.png" /> : ""}
+                        sortedPosts?.map((post) => {
 
-                                            </div>
-                                        </div>
-                                        <div className=''>
-                                            <motion.button className="bg-teal-700  text-white px-4 py-1 rounded-md text-sm" whileHover={{ scale: 1.2, backgroundColor: "blueviolet", transition: { duration: 0.3 } }} whileTap={{ scale: 0.95 }} onClick={() => handleFollow(post._id)}>Follow</motion.button>
-                                            <motion.button className="bg-teal-700 ml-3 text-white px-4 py-1 rounded-md text-sm" whileHover={{ scale: 1.2, transition: { duration: 0.3 } }} whileTap={{ scale: 0.95 }} onClick={() => handleUnfollow(post._id)}>UnFollow</motion.button>
-                                            <motion.button className="bg-purple-600 ml-3 text-white px-4 py-1 rounded-md text-sm" whileHover={{ scale: 1.2, transition: { duration: 0.3 } }} whileTap={{ scale: 0.95 }} onClick={() => handleUpvotePost(post._id)}>upvote</motion.button>
-                                            <motion.button className="bg-red-400 ml-3 text-white px-4 py-1 rounded-md text-sm" whileHover={{ scale: 1.2, transition: { duration: 0.3 } }} whileTap={{ scale: 0.95 }} onClick={() => handleDownVotePost(post._id)}>downVote</motion.button>
-                                        </div>
-                                    </div>
+                            const isFollowing = userData?.data?.following?.some(followingUserId => followingUserId.id === post.userId)
 
-                                    {/* Post Content */}
-                                    <div className="mb-4">
-                                        <h3 className="font-semibold text-emerald-800 text-lg">{post.caption}</h3>
-                                        <div
-                                            dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.description) }}
-                                            className="text-sm mt-3"
-                                        />
-                                    </div>
+                            const isUpvoting = posts.data?.upvotes
+                            console.log(isUpvoting)
 
-                                    {/* Post Image */}
-                                    {post.photo && (
-                                        <motion.img alt={post.caption} className="w-full h-96 object-cover rounded-lg mb-4"
-                                            src={post.photo}
-                                            transition={{ ease: "easeIn", duration: 1, type: 'spring', stiffness: 100 }}
-                                            // whileHover={{ scale: 1.1, }}
-                                        />
-                                    )}
 
-                                    {post && post.isPremium === "YES" ? <Elements stripe={stripePromise}><Payment /></Elements> : null}
+                            return (
 
-                                    {/* Comment Section */}
-                                    <div className="mt-4">
-                                        <input
-                                            className="w-full border rounded-md px-3 py-2 focus:outline-none"
-                                            placeholder="Write a comment..."
-                                            value={commentText[post._id] || ''}
-                                            onChange={(e) => setCommentText(prev => ({ ...prev, [post._id]: e.target.value }))}
-                                        />
-                                        <button
-                                            className="bg-blue-500 text-white px-4 py-1 rounded-full mt-2"
-                                            onClick={() => handleAddComment(post._id)}
-                                        >
-                                            Add Comment
-                                        </button>
-                                    </div>
-
-                                    {/* Display Comments */}
-                                    <div className="mt-4">
-                                        {post.comments.map((comment) => (
-                                            <div key={comment._id} className="flex justify-between items-center mt-2 border-t pt-2">
-                                                <div className='flex gap-4'>
-                                                    <Avatar src={comment?.userProfilePhoto} />
-                                                    <p className=' mt-2'>{comment.text}</p>
+                                <div key={post._id}>
+                                    <div
+                                        className=" shadow-md mt-20 border border-purple-200 rounded-lg p-4 max-w-screen-md"
+                                    >
+                                        <div className="flex justify-between items-center mb-4">
+                                            <div className="flex items-center space-x-3">
+                                                <img
+                                                    alt="User avatar"
+                                                    className="w-10 h-10 rounded-full"
+                                                    src={post.userProfilePhoto}
+                                                />
+                                                <div>
+                                                    <h4 className="">{post.userName}</h4>
+                                                    <p className="text-xs">{new Date(post.createdAt).toLocaleString()}</p>
+                                                    <p className="text-xs">total upvotes {post?.totalUpvotes}</p>
+                                                    <p className="text-xs">total downvote {post?.totalDownvotes}</p>
                                                 </div>
                                                 <div>
-                                                    {user?._id != comment.userId ? '' : <button className="text-blue-500" onClick={() => openEditModal(post._id, comment._id, comment.text)}>
-                                                        Edit
-                                                    </button>}
-                                                    {user?._id != comment.userId ? '' : <button
-                                                        className="text-red-500 ml-2"
-                                                        onClick={() => handleDeleteComment(post._id, comment._id)}
-                                                    >
-                                                        Delete
-                                                    </button>}
+                                                    {post?.isPremium === "YES" ? <img alt="" className='w-12 h-12 ' src="https://i.ibb.co.com/jM3xrDW/premium-quality.png" /> : ""}
+
                                                 </div>
                                             </div>
-                                        ))}
+                                            <div className=''>
+
+                                                {isFollowing ? (
+                                                    <motion.button className="bg-teal-700 ml-3 text-white px-4 py-1 rounded-md text-sm" whileHover={{ scale: 1.2, transition: { duration: 0.3 } }} whileTap={{ scale: 0.95 }} onClick={() => handleUnfollow(post._id)}>UnFollow</motion.button>)
+                                                    :
+                                                    (<motion.button className="bg-teal-700  text-white px-4 py-1 rounded-md text-sm" whileHover={{ scale: 1.2, backgroundColor: "blueviolet", transition: { duration: 0.3 } }} whileTap={{ scale: 0.95 }} onClick={() => handleFollow(post._id)}>Follow</motion.button>)}
+                                                <motion.button className="bg-purple-600 ml-3 text-white px-4 py-1 rounded-md text-sm" whileHover={{ scale: 1.2, transition: { duration: 0.3 } }} whileTap={{ scale: 0.95 }} onClick={() => handleUpvotePost(post._id)}>upvote</motion.button>
+                                                <motion.button className="bg-red-400 ml-3 text-white px-4 py-1 rounded-md text-sm" whileHover={{ scale: 1.2, transition: { duration: 0.3 } }} whileTap={{ scale: 0.95 }} onClick={() => handleDownVotePost(post._id)}>downVote</motion.button>
+                                            </div>
+                                        </div>
+
+                                        {/* Post Content */}
+                                        <div className="mb-4">
+                                            <h3 className="font-semibold text-emerald-800 text-lg">{post.caption}</h3>
+                                            <div
+                                                dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.description) }}
+                                                className="text-sm mt-3"
+                                            />
+                                        </div>
+
+                                        {/* Post Image */}
+                                        {post.photo && (
+                                            <motion.img alt={post.caption} className="w-full h-96 object-cover rounded-lg mb-4"
+                                                src={post.photo}
+                                                transition={{ ease: "easeIn", duration: 1, type: 'spring', stiffness: 100 }}
+                                            // whileHover={{ scale: 1.1, }}
+                                            />
+                                        )}
+
+                                        {post && post.isPremium === "YES" ? <Elements stripe={stripePromise}><Payment /></Elements> : null}
+
+                                        {/* Comment Section */}
+                                        <div className="mt-4">
+                                            <input
+                                                className="w-full border rounded-md px-3 py-2 focus:outline-none"
+                                                placeholder="Write a comment..."
+                                                value={commentText[post._id] || ''}
+                                                onChange={(e) => setCommentText(prev => ({ ...prev, [post._id]: e.target.value }))}
+                                            />
+                                            <button
+                                                className="bg-blue-500 text-white px-4 py-1 rounded-full mt-2"
+                                                onClick={() => handleAddComment(post._id)}
+                                            >
+                                                Add Comment
+                                            </button>
+                                        </div>
+
+                                        {/* Display Comments */}
+                                        <div className="mt-4">
+                                            {post.comments.map((comment) => (
+                                                <div key={comment._id} className="flex justify-between items-center mt-2 border-t pt-2">
+                                                    <div className='flex gap-4'>
+                                                        <Avatar src={comment?.userProfilePhoto} />
+                                                        <p className=' mt-2'>{comment.text}</p>
+                                                    </div>
+                                                    <div>
+                                                        {user?._id != comment.userId ? '' : <button className="text-blue-500" onClick={() => openEditModal(post._id, comment._id, comment.text)}>
+                                                            Edit
+                                                        </button>}
+                                                        {user?._id != comment.userId ? '' : <button
+                                                            className="text-red-500 ml-2"
+                                                            onClick={() => handleDeleteComment(post._id, comment._id)}
+                                                        >
+                                                            Delete
+                                                        </button>}
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                        ))}
+                                </div>)
+                        })}
                 </motion.div>
                 {modalVisible && (
                     <div className="fixed inset-0 z-50 flex items-center justify-cente bg-opacity-50">
--- src/hooks/auth.hook.ts
+++ src/hooks/auth.hook.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {  useMutation, useQuery,  } from "@tanstack/react-query";
 import { FieldValues } from "react-hook-form";
 import { toast } from "sonner";
 // import { toast } from "react-toastify";
