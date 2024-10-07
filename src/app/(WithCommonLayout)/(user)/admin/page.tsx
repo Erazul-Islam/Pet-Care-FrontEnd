e190c689798c6ac5e6cfbe3e6f2158ff5a58f3cd
Erazul Islam <erazul89@gmail.com>
2024-10-08 01:16:56 +0600

    make admin done

--- src/app/(WithCommonLayout)/(user)/admin/page.tsx
+++ src/app/(WithCommonLayout)/(user)/admin/page.tsx
@@ -9,19 +9,19 @@ import React from 'react';
 
 const Adminpage = () => {
 
-    const {data,refetch} = useGetProfile()
+    const { data, refetch } = useGetProfile()
 
-    const {mutateAsync } = useUpdateUserRole()
+    const { mutateAsync } = useUpdateUserRole()
 
-    const {mutate} = useDeleteUser()
+    const { mutate } = useDeleteUser()
 
-    const handleUpdate = (userId : string) => {
-        mutateAsync({userId})
+    const handleUpdate = (userId: string) => {
+        mutateAsync({ userId })
         refetch()
         // window.location.reload()
     }
 
-    const handleDelete = (userId:string) => {
+    const handleDelete = (userId: string) => {
         mutate(userId)
         refetch()
     }
@@ -29,19 +29,24 @@ const Adminpage = () => {
 
 
     return (
-        <div className='grid lg:grid-cols-3 lg:ml-80 lg:mr-60'>
-            {
-                data?.data?.map((one) => (<div key={one._id}>
-                    <div>
-                        <h1>{one.name}</h1>
-                        <img className='w-40 rounded-sm h-40' src={one.profilePhoto} alt="" />
-                        {
-                            one.role === "USER" ? <Button color='secondary' className='mt-3 ml-7' onClick={() => handleUpdate(one._id)} >Make Admin</Button> : <Button color='secondary' className='mt-3 ml-7'>{one.role}</Button>
-                        } <br />
-                        <Button className='mt-3 ml-7' color='warning'  onClick={() => handleDelete(one._id)}>Delete</Button>
-                    </div>
-                </div>))
-            }
+        <div>
+            <div className='grid lg:grid-cols-3 lg:ml-80 lg:mr-60'>
+                {
+                    data?.data?.map((one) => (<div key={one._id}>
+                        <div>
+                            <h1>{one.name}</h1>
+                            <img className='w-40 rounded-sm h-40' src={one.profilePhoto} alt="" />
+                            {
+                                one.role === "USER" ? <Button color='secondary' className='mt-3 ml-7' onClick={() => handleUpdate(one._id)} >Make Admin</Button> : <Button color='secondary' className='mt-3 ml-7'>{one.role}</Button>
+                            } <br />
+                            <Button className='mt-3 ml-7' color='warning' onClick={() => handleDelete(one._id)}>Delete</Button>
+                        </div>
+                    </div>))
+                }
+            </div>
+            <div>
+                
+            </div>
         </div>
     );
 };
