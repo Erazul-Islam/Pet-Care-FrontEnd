2420a11e813e3f282782cca32315017dedf8263d
Erazul Islam <erazul89@gmail.com>
2025-03-12 16:01:06 +0600

    replaced next ui select

--- src/components/posts/get.post.tsx
+++ src/components/posts/get.post.tsx
@@ -400,69 +400,14 @@ const GetPost = () => {
                       </Elements>
                     ) : null}
 
-                    <CommentSection commentText={commentText} setCommentText={setCommentText} handleAddComment={handleAddComment} post={post} openEditModal={openEditModal} handleDeleteComment={handleDeleteComment} />
-
-                    {/* Comment Section */}
-                    {/* <div className="mt-4">
-                      <input
-                        className="w-full border rounded-md px-3 py-2 focus:outline-none"
-                        placeholder="Write a comment..."
-                        value={commentText[post._id] || ""}
-                        onChange={(e) =>
-                          setCommentText((prev) => ({
-                            ...prev,
-                            [post._id]: e.target.value,
-                          }))
-                        }
-                      />
-                      <button
-                        className="bg-pink-700  text-white px-4 py-1 rounded-sm mt-4"
-                        onClick={() => handleAddComment(post._id)}
-                      >
-                        Add Comment
-                      </button>
-                    </div> */}
-
-                    {/* Display Comments */}
-                    {/* <div className="mt-4">
-                      {post.comments.map((comment: TComment) => (
-                        <div
-                          key={comment._id}
-                          className="flex justify-between items-center mt-2 border-t pt-2"
-                        >
-                          <div className="flex gap-4">
-                            <Avatar src={comment?.userProfilePhoto} />
-                            <p className="mt-2">{comment.text}</p>
-                          </div>
-                          <div>
-                            {user?._id !== comment.userId ? null : (
-                              <>
-                                <button
-                                  className="text-blue-500"
-                                  onClick={() =>
-                                    openEditModal(
-                                      post._id,
-                                      comment._id,
-                                      comment.text
-                                    )
-                                  }
-                                >
-                                  Edit
-                                </button>
-                                <button
-                                  className="text-red-500 ml-2"
-                                  onClick={() =>
-                                    handleDeleteComment(post._id, comment._id)
-                                  }
-                                >
-                                  Delete
-                                </button>
-                              </>
-                            )}
-                          </div>
-                        </div>
-                      ))}
-                    </div> */}
+                    <CommentSection
+                      commentText={commentText}
+                      setCommentText={setCommentText}
+                      handleAddComment={handleAddComment}
+                      post={post}
+                      openEditModal={openEditModal}
+                      handleDeleteComment={handleDeleteComment}
+                    />
                   </div>
                 </div>
               );
--- src/components/posts/post.sort.tsx
+++ src/components/posts/post.sort.tsx
@@ -1,34 +1,47 @@
 /* eslint-disable prettier/prettier */
+import { Select, SelectItem } from "@nextui-org/react";
 import React from "react";
 
-const PetPostSort = ({sortBy,handleSortChange,filterByCategory,setFilterByCategory}) => {
+const PetPostSort = ({
+  sortBy,
+  handleSortChange,
+  filterByCategory,
+  setFilterByCategory,
+}) => {
   return (
-    <div className="flex mt-4 gap-20">
-      <div className="flex w-full max-w-screen-md mb-1">
-        <select
-          className="border bg-purple-600 rounded-md px-3 py-2"
-          id="sort"
-          value={sortBy}
-          onChange={handleSortChange}
-        >
-          <option value="newest">Newest</option>
-          <option value="mostUpvoted">Most Upvoted</option>
-        </select>
-      </div>
-      <div className="flex  w-full max-w-screen-md mb-1">
-        <select
-          className="border bg-purple-600 rounded-md px-3 py-2"
-          id="filter"
-          value={filterByCategory}
-          onChange={(e) =>
-            setFilterByCategory(e.target.value as "All" | "Story" | "TIP")
-          }
-        >
-          <option value="All">All</option>
-          <option value="Story">Story</option>
-          <option value="TIP">TIP</option>
-        </select>
-      </div>
+    <div className="flex mt-4 mb-5 gap-20">
+      <Select
+        style={{ borderRadius: "6px", width: 200 }}
+        value={sortBy}
+        onChange={handleSortChange}
+        placeholder="Select "
+      >
+        <SelectItem key={"newest"} value="newest">
+          Newest
+        </SelectItem>
+        <SelectItem key={"mostUpvoted"} value="mostUpvoted">
+          Most Upvoted
+        </SelectItem>
+      </Select>
+
+      <Select
+        placeholder="Select by category"
+        style={{ borderRadius: "6px", width: 200 }}
+        value={filterByCategory}
+        onChange={(e) =>
+          setFilterByCategory(e.target.value as "All" | "Story" | "TIP")
+        }
+      >
+        <SelectItem key="All" value="All">
+          All
+        </SelectItem>
+        <SelectItem key="Story" value="Story">
+          Story
+        </SelectItem>
+        <SelectItem key="TIP" value="TIP">
+          TIP
+        </SelectItem>
+      </Select>
     </div>
   );
 };
