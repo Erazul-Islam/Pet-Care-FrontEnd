3c3668e95f7cb225621046a1e850eae9d7f93d54
Erazul Islam <erazul89@gmail.com>
2025-03-13 16:31:25 +0600

    showed the following

--- src/app/(WithCommonLayout)/user-profile/[id]/page.tsx
+++ src/app/(WithCommonLayout)/user-profile/[id]/page.tsx
@@ -12,31 +12,49 @@ import { TFollower } from "@/src/types";
 const Page = () => {
   const params = useParams();
   const id = params.id;
-  const { data,isLoading } = useGetSignleProfile(id as string);
-  const info = data?.data 
-  console.log(info)
-  const followers = info?.followers
-  const following = info?.following
+  const { data, isLoading } = useGetSignleProfile(id as string);
+  const info = data?.data;
+  console.log(info);
+  const followers = info?.followers;
+  const following = info?.following;
 
-
-  return <Card className=" lg:w-[300px] rounded-md ml-10 mr-6">
-            <CardHeader className="text-center justify-center items-center">
-                { info?.name }
-            </CardHeader>
-            <CardBody>
-                <Image src={info?.profilePhoto} width={300} height={300} alt="Profile Photo" />
-                <div>
-                <p> Followers </p>
-                    {followers?.map((follower : TFollower) => <div key={follower.id}>
-                       <div>
-                       <p>{follower.username}</p>
-                       <Image src={follower?.profilePhoto} width={100} height={100} alt="Follwer profile photo" />
-                       </div>
-                         
-                     </div> )}
-                </div>
-            </CardBody>
-  </Card>;
+  return (
+    <Card className=" lg:w-[300px] rounded-md ml-10 mr-6">
+      <CardHeader className="text-center justify-center items-center">
+        {info?.name}
+      </CardHeader>
+      <CardBody>
+        <Image
+          src={info?.profilePhoto}
+          width={300}
+          height={300}
+          alt="Profile Photo"
+        />
+        {/* Follower */}
+        <div>
+          <p> Followers </p>
+          {followers?.map((follower: TFollower) => (
+            <div key={follower.id}>
+              <div>
+                <p>{follower.username}</p>
+                <Image
+                  src={follower?.profilePhoto}
+                  width={100}
+                  height={100}
+                  alt="Follwer profile photo"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        {/* Following */}
+        <div>
+            <p>Following</p>
+            {following?.map((following : TFollower) => <div key={following?.id}> <p>{following?.username}</p>  </div> )}
+        </div>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default Page;
