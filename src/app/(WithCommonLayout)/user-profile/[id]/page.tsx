76f95c371ed8bab263f69315d04d3ae699005504
Erazul Islam <erazul89@gmail.com>
2025-03-13 15:56:12 +0600

    showed the cloudinary image

--- src/app/(WithCommonLayout)/user-profile/[id]/page.tsx
+++ src/app/(WithCommonLayout)/user-profile/[id]/page.tsx
@@ -1,18 +1,28 @@
 /* eslint-disable prettier/prettier */
 "use client";
 
-import { useGetSignleProfile } from "@/src/hooks/auth.hook";
-
+import { Card, CardBody, CardHeader, } from "@nextui-org/react";
 import { useParams } from "next/navigation";
 import React from "react";
+
+import { useGetSignleProfile } from "@/src/hooks/auth.hook";
+import Image from "next/image";
 
 const Page = () => {
   const params = useParams();
   const id = params.id;
-  const { data } = useGetSignleProfile(id as string);
-  console.log(data)
+  const { data,isLoading } = useGetSignleProfile(id as string);
+  const info = data?.data 
+  console.log(info)
 
-  return <div>this is {params.id}</div>;
+  return <Card className=" lg:w-[300px] rounded-md ml-10 mr-6">
+            <CardHeader className="text-center justify-center items-center">
+                { info?.name }
+            </CardHeader>
+            <CardBody>
+                <Image src={info?.profilePhoto} width={100} height={100} alt="Profile Photo" />
+            </CardBody>
+  </Card>;
 };
 
 export default Page;
